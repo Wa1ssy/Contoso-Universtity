c31ad509f52a69e25e3eafa79e4bd500054f0b77
opilane <opilane@ORDI-402607>
2024-09-19 08:40:07 +0300

    Edit button

--- ContosoUniverstity/Controllers/StudentsController.cs
+++ ContosoUniverstity/Controllers/StudentsController.cs
@@ -145,7 +145,57 @@ namespace ContosoUniverstity.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-       
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        private bool StudentExists(int id)
+        {
+            throw new NotImplementedException();
+        }
 
         public async Task<IActionResult> Clone(int id)
         {
