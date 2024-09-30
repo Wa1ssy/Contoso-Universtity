458729c40c41611c5279a08d2d1738be3e095c1c
opilane <opilane@HP-CZC3217069>
2024-09-30 15:33:07 +0300

    Department create

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a5288f28e5490c8f3d0ec10b67e1747a845ccc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67034810ef69a231976697459a587a7abd052692")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
