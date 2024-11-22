86aac4831c66622ff9f503d473759d922259c9b1
opilane <opilane@HP-CZC321705F>
2024-11-22 13:06:21 +0200

    sdfsdf

--- ContosoUniverstity/Controllers/DepartmentsController.cs
+++ ContosoUniverstity/Controllers/DepartmentsController.cs
@@ -59,6 +59,38 @@ namespace ContosoUniverstity.Controllers
             ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "FullName");
             return View(department);
         }
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department != null)
+            {
+                _context.Departments.Remove(department);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
 
     }
 }
--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a79cc9eb83cc80c59df7e2960d9444c006ab14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8ca091841e79de2158819d1cf7d66a79e40f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
