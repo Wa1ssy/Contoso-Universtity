3aacb15167c32012af9a35686ce2152c991d5344
opilane <opilane@HP-CZC321706P>
2025-02-04 11:26:29 +0200

    Courses views tehtud

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b4c001d094386dd5040669457d08912a42faa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b9d798fa7ea61f484cb5aa7637a7f926b55aff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
