caf85f5685a215f8b9d364ae1b42922f723ada45
Opilane TTHK <opilane@tthk.ee>
2025-02-11 16:26:02 +0200

    Deleted departments Delete Edit and BaseOn

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e980cdcb3b9dd6ca5222744c2f124869b916387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e2aac428276a8f4644199968c1210c7c9a9f79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
