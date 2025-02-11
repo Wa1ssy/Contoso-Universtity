a7a0f90057779e98264843b05051076ad5adb066
Opilane TTHK <opilane@tthk.ee>
2025-02-11 17:19:18 +0200

    Delete method

--- ContosoUniverstity/Controllers/CoursesController.cs
+++ ContosoUniverstity/Controllers/CoursesController.cs
@@ -32,4 +32,33 @@ public class CoursesController : Controller
         return View("DetailsDelete", course);
     }
 
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var course = await _context.Courses
+            .Include(c => c.Enrollments)
+            .FirstOrDefaultAsync(m => m.CourseID == id);
+
+        if (course == null) return NotFound();
+
+        ViewData["IsDeleteView"] = true;
+        return View("DetailsDelete", course);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+
+        if (course != null)
+        {
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
 }
