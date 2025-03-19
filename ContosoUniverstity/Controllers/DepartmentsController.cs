edf95b4a882b2abf24b1f517f8cef773402d611b
Wa1ssy <132674823+Wa1ssy@users.noreply.github.com>
2025-03-19 18:44:19 +0200

    Delete method

--- ContosoUniverstity/Controllers/DepartmentsController.cs
+++ ContosoUniverstity/Controllers/DepartmentsController.cs
@@ -39,8 +39,8 @@ namespace ContosoUniverstity.Controllers
         }
 
 
-    
-    [HttpGet]
+
+        [HttpGet]
         public IActionResult Create()
         {
             ViewData["InstructorID"] = new SelectList(_context.Instructors, "Id", "FullName");
@@ -60,9 +60,39 @@ namespace ContosoUniverstity.Controllers
             ViewData["InstructorID"] = new SelectList(_context.Instructors, "Id", "FullName", department.InstructorId);
             return View(department);
         }
-        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.DepartmentID == id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
 }
 
 
